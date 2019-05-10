17d01881337c257014911d94a99ed82f80b13d4a
jasona123456 <50482199+jasona123456@users.noreply.github.com>
2019-05-10 17:06:41 +0800

    Add files via upload

---
+++ Blog.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" sizes="30x30" type="img/png" href="image/qwe1.png" >
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
+	<title>Jason's Blog</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/Blog.css">
+<body>
+    <div class= "wrapper">
+
+    	<nav class="nav">
+    		 <script type="text/javascript">
+				$(window).on('scroll', function() {
+					if($(window).scrollTop()) {
+						$('nav').addClass('white'); 
+						}
+						else {
+							$('nav').removeClass('white');
+			}
+			})
+			 </script>
+			 <script type="text/javascript">
+			 	$(window).on('scroll', function() {
+			 		if($(window).scrollTop()) {
+			 			$(" .logo img").attr("src", "image/qwewhite.png"); 
+			 		}
+			 		else {
+			 			$(" .logo img").attr("src", "image/qwe.png"); 
+			 		}
+			 	})
+			 </script>
+    		<div class ="logo"><img src="image/qwe.png" ></div>
+    		<ul class="container green borderXwidth">
+    			
+    			<li><a href="Blog.php">Home</a></li>
+    			<li><a href="Blog1.php">About</a></li></div>
+    			
+    		</ul>
+    	</nav>
+
+    	<div class="content-wrapper">
+
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+
+    		<h1 style=font-size:80px>Top 5 Richest People in the World in 2019 </h1>
+    	<br>
+    	<br>
+    	
+    		<h2 style="font-size:50px ">5. Amancio Ortega</h2>
+    	<div class="amancio">
+    		<img src="image/amancio.jpg" alt="Amancio Ortega"> 
+    		<h3 style="font-size: 30px; color: #000">Net Worth: $67 Billion</h3>
+    	</div>
+
+    	<div>
+    		<p style="font-size: 20px; color: #000">Amancio Ortega is a Spanish entrepreneur and founder of the Inditex fashion group. Inditex owns Zara, and Zara is one of the most popular clothing chains worldwide.
+    	
+			Ortega recently moved back up to the 5th richest man in the world, after other players on the list have dropped down slightly. However, Ortega’s also lost over $11 billion from his net worth in recent months.</p>
+
+		<br>
+    	<br>
+    	<br>
+
+    		<h2 style="font-size:50px ">4. Warren Buffett</h2>
+    	
+    			<img src="image/Warren.jpg" alt="Warren Buffett"> <h3 style="font-size: 30px; color: #000">Net Worth: $82.7 Billion</h3>
+
+    				<p style="font-size: 20px; color: #000 ">Warren Buffett is an American entrepreneur and investor, and CEO of Berkshire Hathaway. Buffett is the most successful investor worldwide, and millions of other traders look up to him because of his success.
+
+					Warren Buffett is the 4th richest man in the world, with an incredible net worth of $82.7 billion. However, despite this fact, Buffett still lives in the home he purchased back when he didn’t even know what a billion dollars looked like.</p>
+		<br>
+    	<br>
+    	<br>
+			<h2 style="font-size:50px ">3. Bernard Arnault</h2>
+
+		<img src="image/Arnault.jpg" alt="Bernard Arnault"> 
+
+			<h3 style="font-size: 30px; color: #000">Net Worth: $83.7 Billion</h3>
+
+				<p style="font-size: 20px; color: #000 ">Bernard Arnault is the CEO of LVMH (Louis Vitton Moet Hennessey).
+
+					Ultimately, the company was formed from merging the two businesses together. Louis Vitton is the very successful luxury fashion brand, and Moet Hennessey is the merged champagne and cognac manufacturers.
+
+					Bernard has been making serious movements up this list in the past year. In fact, at the beginning of 2017, he was at 14th. Now, a year later, he’s placed at 3rd on the list of the 20 richest people on earth in 2019. </p>
+
+		<br>
+    	<br>
+    	<br>
+
+    		<h2 style="font-size:50px ">2. Bill Gates</h2>
+    			
+    			<img src="image/Bill.jpg" alt="Bill Gates"> 
+
+    		<h3 style="font-size: 30px; color: #000">Net Worth: $98.3 Billion</h3>
+
+    			<p style="font-size: 20px; color: #000 ">Bill Gates is the founder of Microsoft, and was the richest man in the world for several years running. Gates founded Microsoft in 1975 with Paul Allen, and has since become the largest PC software company worldwide.
+
+				Last year, Bill Gates has gained an incredible $10 billion in additional net worth! This has put him at an estimated net worth of $98.3 billion overall. However, he’s recently been overtaken by the next man on this list…</p>
+		<br>
+		<br>
+		<br>
+
+			<h2 style="font-size:50px ">1. Jeff Bezos</h2>
+    			
+    			<img src="image/jeff.jpg" alt="Jeff Bezos"> 
+
+    		<h3 style="font-size: 30px; color: #000">Net Worth: $145.3 Billion</h3>
+
+			<p style="font-size: 20px; color: #000 ">Jeff Bezos is the founder of Amazon, one of the biggest and most popular companies on the web. Amazon started as a simple online bookstore in Jeff’s bedroom, and the initial sales were slow.
+
+			In 2019, Jeff Bezos has become the richest man in the world, after adding a whopping $40 billion to his net worth last year! Amazon is making major moves, after recently acquiring wholefoods, launching drone shipping and a bunch of other strong ideas.
+
+			Jeff Bezos is the first man ever to reach the $100 billion mark, which is just insane! Keep reading below to view a complete summary of the richest people in 2019. </p>
+
+			<br>
+		<br>
+		<br>
+		<br>
+		<br>
+																							
+		
+		<p> q</p>
+    </div>
+</div>
+</body>
+</html>
---
+++ Blog1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jason's Blog</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" sizes="30x30" type="img/png" href="image/qwe1.png" >
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="css/Blog1.css">
+<body>
+    <div class= "wrapper">
+
+    	<nav class="nav">
+    		 <script type="text/javascript">
+				$(window).on('scroll', function() {
+					if($(window).scrollTop()) {
+						$('nav').addClass('white'); 
+						}
+						else {
+							$('nav').removeClass('white');
+			}
+			})
+			 </script>
+			 <script type="text/javascript">
+			 	$(window).on('scroll', function() {
+			 		if($(window).scrollTop()) {
+			 			$(" .logo img").attr("src", "image/qwewhite.png"); 
+			 		}
+			 		else {
+			 			$(" .logo img").attr("src", "image/qwe.png"); 
+			 		}
+			 	})
+			 </script>
+    		<div class ="logo"><img src="image/qwe.png" ></div>
+    		<ul class="container green borderXwidth">
+    			
+    			<li><a href="Blog.php">Home</a></li>
+    			<li><a href="Blog1.php">About</a></li></div>
+
+    			
+    				
+    			
+    			</div>
+    			</div>
+    		</ul>
+    	</nav>
+</head>
+<body>
+	<div class="content-wrapper"> 
+    				
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+
+    					<div class="jason">
+						<img src="image/jason.jpg" alt="Jason Giron">
+						</div>
+	
+						<div class="jas">
+    					<h1 style="font-size: 50px">Jason Giron </h1>
+    					<h2 style="font-size: 40px">ICT C - Isaac</h2>
+    					<h3 style="font-size: 20px"> I'm jason giron a well known student in pcu, <br> I'm good, honorable mention as you can see at my report card <br>never been bullied nor bully someone, <br> Bassist of a band <br> you can call me in this number 09457459745 <br> Charoot
+    					</div>	
+    				
+    					
+
+    							<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<br>
+    					<h5 style="color: #fff">.</h5>
+    			</div>	
+    				
+</body>
+</html>
